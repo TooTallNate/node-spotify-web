4a9d3a1ae6a0da769febba33cf0827ad7c0b7d44
Nathan Rajlich <nathan@tootallnate.net>
2013-01-19 15:20:14 -0800

    example: refactor playAlbum.js example to use new APIs

--- example/playAlbum.js
+++ example/playAlbum.js
@@ -9,50 +9,41 @@ var Spotify = require('../');
 var login = require('../login');
 var lame = require('lame');
 var Speaker = require('speaker');
-var superagent = require('superagent');
 
+// determine the URI to play, ensure it's an "album" URI
 var uri = process.argv[2] || 'spotify:album:7u6zL7kqpgLPISZYXNTgYk';
+var type = Spotify.uriType(uri);
+if ('album' != type) {
+  throw new Error('Must pass a "album" URI, got ' + JSON.stringify(type));
+}
 
 Spotify.login(login.username, login.password, function (err, spotify) {
   if (err) throw err;
 
   // first get a "Album" instance from the album URI
-  spotify.metadata(uri, function (err, album) {
+  spotify.get(uri, function (err, album) {
     if (err) throw err;
 
-    // first get the individual track IDs
-    var trackIds = [];
+    // first get the Track instances for each disc
+    var tracks = [];
     album.disc.forEach(function (disc) {
       if (!Array.isArray(disc.track)) return;
-      var ids = disc.track.map(function (track) { return track.gid; });
-      trackIds.push.apply(trackIds, ids);
+      tracks.push.apply(tracks, disc.track);
     });
-    //console.log(trackIds);
-
-    var trackUris = trackIds.map(function (id) {
-      return Spotify.gid2uri('track', id);
-    });
-    console.log(trackUris);
+    console.log(tracks.map(function(t){ return t.uri; }));
 
     function next () {
-      var uri = trackUris.shift();
-      if (!uri) return spotify.disconnect();
+      var track = tracks.shift();
+      if (!track) return spotify.disconnect();
 
-      spotify.metadata(uri, function (err, track) {
+      track.get(function (err) {
         if (err) throw err;
+        console.log('Playing: %s - %s', track.artist[0].name, track.name);
 
-        spotify.trackUri(track, function (err, res) {
-          if (err) throw err;
-          console.log('Playing: %s - %s', track.artist[0].name, track.name);
-          console.log('MP3 URL: %j', res.uri);
-
-          superagent.get(res.uri)
-            .pipe(new lame.Decoder())
-            .pipe(new Speaker())
-            .on('finish', function () {
-              spotify.sendTrackEnd(res.lid, uri, track.duration, next);
-            });
-        });
+        track.play()
+          .pipe(new lame.Decoder())
+          .pipe(new Speaker())
+          .on('finish', next);
       });
     }
     next();
