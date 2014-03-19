f0c60a8a2e7522a1d8a60b32193b97cee6da7b42
Sam Billingham <sam@harkable.com>
2014-03-19 14:21:31 +0000

    changed rootlist to publishedrootlist to resolve #34

--- example/rootlist.js
+++ example/rootlist.js
@@ -1,17 +1,20 @@
 
 /**
- * Gets the user's "rootlist" (array of playlist IDs).
+ * Gets a user's "rootlist" (array of playlist IDs).
  */
 
 var Spotify = require('../');
 var login = require('../login');
 
+// determine which Users rootlist to select
+var user = process.argv[2] || login.username;
+
 // initiate the Spotify session
 Spotify.login(login.username, login.password, function (err, spotify) {
   if (err) throw err;
 
-  // get the currently logged in user's rootlist (playlist names)
-  spotify.rootlist(function (err, rootlist) {
+  // get the selected user's rootlist (playlist names)
+  spotify.rootlist( user, function (err, rootlist) {
     if (err) throw err;
 
     console.log(rootlist.contents);
